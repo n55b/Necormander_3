6eff9fc5c8f02c45ab31e363f31617c5d74e0a17
IdfKLeh <rimmy68@naver.com>
2026-04-14 13:42:40 +0900

    일단 반만 수정

---
+++ Assets/Scripts/SOData/Define/ThrownStateSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ThrownState", menuName = "FSM/State/Thrown")]
+public class ThrownStateSO : FSMStateSO
+{
+    public override void Enter(EntityFSM fsm) { }
+    public override void Execute(EntityFSM fsm) { }
+    public override void Exit(EntityFSM fsm) { }
+}
