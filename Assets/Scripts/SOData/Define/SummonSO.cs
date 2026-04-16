1b33496ea89cc08afd7beb483e330ba1d81353c0
n55b <96624341+n55b@users.noreply.github.com>
2026-04-16 15:57:28 +0900

    2티어 소환수 추가

--- Assets/Scripts/SOData/Define/SummonSO.cs
+++ Assets/Scripts/SOData/Define/SummonSO.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class SummonSO : ScriptableObject
 {
     [SerializeField] private GameObject summonAlly;
-    [TextArea(1, 1)]
+    [TextArea(1, 2)]
     public string note = "[1티어 소환수]\n1. 전사 || 2. 아처 || 3. 방패병 || 4. 사제";
     [SerializeField] private CommandData summonCommand;
 
