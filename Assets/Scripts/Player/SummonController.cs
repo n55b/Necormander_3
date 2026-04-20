6859f0d42a81452274d2e8640805182815daf7ef
IdfKLeh <rimmy68@naver.com>
2026-04-20 13:43:19 +0900

    소환 시스템 수정

--- Assets/Scripts/Player/SummonController.cs
+++ Assets/Scripts/Player/SummonController.cs
@@ -6,45 +6,59 @@ public class SummonController : MonoBehaviour
 {
     public LayerMask obstacleLayer; // 벽이나 장애물 레이어 설정
     
-    // 현재 눌려 있는 소환 키와 조합 키 상태
-    private bool _isTabPressed = false;
-    private int _pressedNumKey = -1; // -1: 안 눌림, 1-4: 숫자 키
+    // 현재 선택 상태 관리
+    private int _selectionStep = 0; // 0: 대기, 1: 1단계(기본), 2: 2단계(확장)
+    private int _selectedNum = -1;  // 선택된 숫자 (1-4)
 
-    public bool IsSummoningMode => _pressedNumKey != -1;
+    public bool IsSummoningMode => _selectionStep > 0;
 
-    public void OnTab(InputAction.CallbackContext context)
-    {
-        if (context.performed) _isTabPressed = true;
-        else if (context.canceled) _isTabPressed = false;
-    }
+    // 더 이상 Tab 키는 소환에 관여하지 않으므로 제거하거나 비워둠
+    public void OnTab(InputAction.CallbackContext context) { }
 
     public void OnNumKey(int num, InputAction.CallbackContext context)
     {
-        if (context.performed)
+        // 키가 실제로 눌린 시점(Value > 0)에만 로직 실행
+        // performed가 키를 뗄 때도 호출될 수 있으므로 이를 방지함
+        if (context.performed && context.ReadValueAsButton())
         {
-            _pressedNumKey = num;
-            Debug.Log($"<color=cyan>[SummonController]</color> 소환 모드 활성화: {num}번 키 누름");
-        }
-        else if (context.canceled)
-        {
-            if (_pressedNumKey == num)
+            // 이미 해당 숫자가 선택된 상태에서 또 누르거나, 다른 숫자를 누르면 단계 진행
+            if (_selectionStep == 0)
             {
-                _pressedNumKey = -1;
-                Debug.Log($"<color=yellow>[SummonController]</color> 소환 모드 해제");
+                _selectionStep = 1;
+                _selectedNum = num;
+                Debug.Log($"<color=cyan>[SummonController]</color> 1단계 소환 모드: {GetMinionName(num, 1)} 선택");
+            }
+            else if (_selectionStep == 1)
+            {
+                _selectionStep = 2;
+                _selectedNum = num;
+                Debug.Log($"<color=cyan>[SummonController]</color> 2단계 소환 모드: {GetMinionName(num, 2)} 선택");
+            }
+            else // 2단계에서 또 누르면 다시 2단계 유지하며 종류만 변경 (사용자 요청: "두번째 누른 애만 종류에 영향")
+            {
+                _selectedNum = num;
+                Debug.Log($"<color=cyan>[SummonController]</color> 2단계 종류 변경: {GetMinionName(num, 2)} 선택");
             }
         }
+    }
+
+    // 소환 모드 강제 종료 (소환 후 호출)
+    public void ResetSummonMode()
+    {
+        _selectionStep = 0;
+        _selectedNum = -1;
+        Debug.Log($"<color=yellow>[SummonController]</color> 소환 모드 해제 (줍기 모드로 복귀)");
     }
 
     // 현재 선택된 미니언 타입 반환
     public CommandData GetCurrentSelectedType()
     {
-        // 소환 모드가 아니면 기본적으로 Warrior 반환 (실제로는 IsSummoningMode 체크 후 사용됨)
-        if (_pressedNumKey == -1) return CommandData.SkeletonWarrior;
+        if (_selectionStep == 0) return CommandData.SkeletonWarrior;
 
-        if (!_isTabPressed)
+        if (_selectionStep == 1)
         {
-            // Tab 안 눌렸을 때 (1-4)
-            return _pressedNumKey switch
+            // 1단계 (1-4)
+            return _selectedNum switch
             {
                 1 => CommandData.SkeletonWarrior,
                 2 => CommandData.SkeletonShieldbearer,
@@ -55,8 +69,8 @@ public class SummonController : MonoBehaviour
         }
         else
         {
-            // Tab 눌렸을 때 (Tab + 1-4)
-            return _pressedNumKey switch
+            // 2단계 (Combo: 1-4)
+            return _selectedNum switch
             {
                 1 => CommandData.SkeletonBomber,
                 2 => CommandData.SkeletonSpearman,
@@ -64,6 +78,18 @@ public class SummonController : MonoBehaviour
                 4 => CommandData.SkeletonThief,
                 _ => CommandData.SkeletonBomber
             };
+        }
+    }
+
+    private string GetMinionName(int num, int step)
+    {
+        if (step == 1)
+        {
+            return num switch { 1 => "전사", 2 => "방패병", 3 => "궁수", 4 => "사제", _ => "" };
+        }
+        else
+        {
+            return num switch { 1 => "폭탄병", 2 => "창병", 3 => "마법사", 4 => "도적", _ => "" };
         }
     }
 
